c593240be83363f6da4665e7be185476fb6d7789
mindmentors <168081457+mindmentors@users.noreply.github.com>
2024-05-10 02:22:10 +0530

    initial commit

---
+++ apps/ecommerce-shop/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mmc-eshop-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'ecommerce-shop';
+}
---
+++ apps/ecommerce-shop/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { AccordionModule } from 'primeng/accordion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {UiModule} from '@mmc-eshop/ui'
+
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'products', component: ProductListComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    HomePageComponent,
+    ProductListComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes), UiModule,AccordionModule, BrowserAnimationsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
